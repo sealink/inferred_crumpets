cf8b472c3c8e7c0e46e9532649115b382cf1ad4b
Sean Chamberlain <seanlchamberlain@gmail.com>
2017-09-12 11:14:33 +0930

    Tests

--- spec/inferred_crumpets/view_helpers_spec.rb
+++ spec/inferred_crumpets/view_helpers_spec.rb
@@ -19,8 +19,6 @@ RSpec.describe InferredCrumpets::ViewHelpers do
   }
 
   let(:view_context) { MockView.new(:action => action) }
-  # let(:objects_path) { '/users' }
-  let(:parent) { nil }
   let(:new_record) { false }
   let(:id) { new_record ? nil : 1 }
 
@@ -41,6 +39,10 @@ RSpec.describe InferredCrumpets::ViewHelpers do
       class Relation; end
     end
 
+    class User < ActiveRecord::Base; end
+    class Person < ActiveRecord::Base; end
+    class Organisation < ActiveRecord::Base; end
+
     stub_const 'ActionController::RoutingError', StandardError
 
     allow(view_context).to receive(:objects_path) { nil }
@@ -52,15 +54,16 @@ RSpec.describe InferredCrumpets::ViewHelpers do
     allow(view_context).to receive(:url_for).with(action: :index, controller: 'users').and_return('/users')
     allow(view_context).to receive(:url_for).with(user).and_return('/users/1')
     allow(view_context).to receive(:url_for).with(action: :show, controller: 'users', id: 1).and_return('/users/1')
+    allow(view_context).to receive(:url_for).with([user.class]).and_return('/users')
   end
 
 
   describe '#build_inferred_crumbs!' do
     subject { view_context.render_inferred_crumbs }
 
-    let(:user_class) { ActiveRecord::Base }
+    let(:user_class) { User }
     let(:users) { ActiveRecord::Relation.new }
-    let(:user) { ActiveRecord::Base.new }
+    let(:user) { User.new }
 
     before do
       allow(user_class).to receive(:table_name).and_return('users')
@@ -95,8 +98,6 @@ RSpec.describe InferredCrumpets::ViewHelpers do
       before do
         allow(view_context).to receive(:collection).and_return(users)
         allow(view_context).to receive(:current_object).and_return(user)
-        allow(view_context).to receive(:parent) { parent }
-        allow(view_context).to receive(:parent_object) { parent }
       end
 
       it 'should infer crumbs: Users / New' do
@@ -110,8 +111,6 @@ RSpec.describe InferredCrumpets::ViewHelpers do
       before do
         allow(view_context).to receive(:collection).and_return(users)
         allow(view_context).to receive(:current_object).and_return(user)
-        # allow(view_context).to receive(:parent) { parent }
-        # allow(view_context).to receive(:parent_object) { parent }
       end
 
       it 'should infer crumbs: Users / Alice' do
@@ -123,6 +122,344 @@ RSpec.describe InferredCrumpets::ViewHelpers do
 
         it 'should infer crumbs: Users / Alice / Edit' do
           expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><a href="/users/1">Alice</a></li><li class="active"><span>Edit</span></li></ul>'
+        end
+      end
+    end
+
+    context 'when can route to action but subject is not linkable' do
+      let(:route_checker_class) { double }
+      let(:route_checker) { double(view_context: view_context) }
+      let(:can_route) { true }
+      let(:linkable) { true }
+      let(:collection_linkable) { true }
+
+      before do
+        stub_const 'InferredCrumpets::RouteChecker', route_checker_class
+
+        allow(route_checker_class).to receive(:new).with(view_context).and_return route_checker
+        allow(route_checker).to receive(:linkable?).with(user).and_return linkable
+        allow(route_checker).to receive(:linkable?).with([User]).and_return collection_linkable
+        allow(route_checker).to receive(:can_route?).and_return can_route
+
+        allow(view_context).to receive(:collection).and_return(users)
+        allow(view_context).to receive(:current_object).and_return(user)
+        allow(view_context).to receive(:url_for).with([user.class]).and_return('/users')
+      end
+
+      context 'on show route' do
+        let(:action) { 'show' }
+
+        it 'should infer crumbs: Users / Alice ' do
+          expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><span>Alice</span></li></ul>'
+        end
+      end
+
+      context 'on show route' do
+        let(:action) { 'edit' }
+
+        it 'should infer crumbs: Users / Alice / Edit ' do
+          expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><a href="/users/1">Alice</a></li><li class="active"><span>Edit</span></li></ul>'
+        end
+      end
+
+      context 'when not linkable' do
+        let(:linkable) { false }
+
+        context 'on show route' do
+          let(:action) { 'show' }
+
+          it 'should infer crumbs: Users / Alice ' do
+            expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><span>Alice</span></li></ul>'
+          end
+        end
+
+        context 'on edit route' do
+          let(:action) { 'edit' }
+
+          it 'should infer crumbs: Users / Alice / Edit ' do
+            expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><span>Alice</span></li><li class="active"><span>Edit</span></li></ul>'
+          end
+        end
+      end
+
+      context 'when collection not linkable' do
+        let(:collection_linkable) { false }
+        let(:linkable) { false }
+
+        context 'on show route' do
+          let(:action) { 'show' }
+
+          it 'should infer crumbs: Users / Alice ' do
+            expect(subject).to eq '<ul class="breadcrumb"><li><span>Users</span></li><li><span>Alice</span></li></ul>'
+          end
+        end
+
+        context 'on edit route' do
+          let(:action) { 'edit' }
+
+          it 'should infer crumbs: Users / Alice / Edit ' do
+            expect(subject).to eq '<ul class="breadcrumb"><li><span>Users</span></li><li><span>Alice</span></li><li class="active"><span>Edit</span></li></ul>'
+          end
+        end
+      end
+
+      context 'when cannot route' do
+        let(:can_route) { false }
+
+        context 'on show route' do
+          let(:action) { 'show' }
+
+          it 'should infer crumbs: Users / Alice ' do
+            expect(subject).to eq '<ul class="breadcrumb"><li><span>Users</span></li><li><span>Alice</span></li></ul>'
+          end
+        end
+
+        context 'on edit route' do
+          let(:action) { 'edit' }
+
+          it 'should infer crumbs: Users / Alice / Edit ' do
+            expect(subject).to eq '<ul class="breadcrumb"><li><span>Users</span></li><li><span>Alice</span></li><li class="active"><span>Edit</span></li></ul>'
+          end
+        end
+      end
+    end
+
+    context 'when subject is single table inherited' do
+      let(:route_checker_class) { double }
+      let(:route_checker) { double(view_context: view_context) }
+      let(:person_class) { Person }
+      let(:person) { Person.new }
+
+      let(:collection_linkable) { true }
+      let(:subject_base_linkable) { true }
+      let(:subject_base_class_linkable) { true }
+      let(:subject_linkable) { true }
+      let(:can_route) { true }
+
+      before do
+        stub_const 'InferredCrumpets::RouteChecker', route_checker_class
+
+        allow(user).to receive(:becomes).and_return(person)
+        allow(user_class).to receive(:base_class).and_return(person_class)
+
+        allow(route_checker_class).to receive(:new).with(view_context).and_return route_checker
+
+        allow(route_checker).to receive(:linkable?).with([User]).and_return collection_linkable
+        allow(route_checker).to receive(:linkable?).with([Person]).and_return subject_base_class_linkable
+        allow(route_checker).to receive(:linkable?).with(person).and_return subject_base_linkable
+        allow(route_checker).to receive(:linkable?).with(user).and_return subject_linkable
+        allow(route_checker).to receive(:can_route?).and_return can_route
+
+        allow(view_context).to receive(:collection).and_return(users)
+        allow(view_context).to receive(:current_object).and_return(user)
+        allow(view_context).to receive(:url_for).with(person_class).and_return('/people')
+        allow(view_context).to receive(:url_for).with(person).and_return('/people/1')
+        allow(view_context).to receive(:url_for).with([Person]).and_return('/people')
+      end
+
+      context 'with no parent' do
+        context 'on show route' do
+          let(:action) { 'show' }
+
+          it 'should infer crumbs: Users / Alice ' do
+            expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><span>Alice</span></li></ul>'
+          end
+        end
+
+        context 'on edit route' do
+          let(:action) { 'edit' }
+
+          it 'should infer crumbs: Users / Alice / Edit' do
+            expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><a href="/users/1">Alice</a></li><li class="active"><span>Edit</span></li></ul>'
+          end
+        end
+
+        context 'when subject is not linkable but base class and collection is' do
+          let(:subject_linkable) { false }
+
+          context 'on show route' do
+            let(:action) { 'show' }
+
+            it 'should infer crumbs: Users / Alice ' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><span>Alice</span></li></ul>'
+            end
+          end
+
+          context 'on edit route' do
+            let(:action) { 'edit' }
+
+            it 'should infer crumbs: Users / Alice / Edit' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/users">Users</a></li><li><span>Alice</span></li><li class="active"><span>Edit</span></li></ul>'
+            end
+          end
+        end
+
+        context 'when subject and collection is not linkable but base class and collection is' do
+          let(:subject_linkable) { false }
+          let(:collection_linkable) { false }
+
+          context 'on show route' do
+            let(:action) { 'show' }
+
+            it 'should infer crumbs: Users / Alice ' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/people">Users</a></li><li><span>Alice</span></li></ul>'
+            end
+          end
+
+          context 'on edit route' do
+            let(:action) { 'edit' }
+
+            it 'should infer crumbs: Users / Alice / Edit' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/people">Users</a></li><li><a href="/people/1">Alice</a></li><li class="active"><span>Edit</span></li></ul>'
+            end
+          end
+        end
+
+        context 'when nothing is linkable' do
+          let(:subject_linkable) { false }
+          let(:collection_linkable) { false }
+          let(:subject_base_linkable) { false }
+          let(:subject_base_class_linkable) { false }
+
+          context 'on show route' do
+            let(:action) { 'show' }
+
+            it 'should infer crumbs: Users / Alice ' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><span>Users</span></li><li><span>Alice</span></li></ul>'
+            end
+          end
+
+          context 'on edit route' do
+            let(:action) { 'edit' }
+
+            it 'should infer crumbs: Users / Alice / Edit' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><span>Users</span></li><li><span>Alice</span></li><li class="active"><span>Edit</span></li></ul>'
+            end
+          end
+        end
+
+        context 'when cannot route' do
+          let(:can_route) { false }
+
+          context 'on show route' do
+            let(:action) { 'show' }
+
+            it 'should infer crumbs: Users / Alice ' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><span>Users</span></li><li><span>Alice</span></li></ul>'
+            end
+          end
+
+          context 'on edit route' do
+            let(:action) { 'edit' }
+
+            it 'should infer crumbs: Users / Alice / Edit' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><span>Users</span></li><li><span>Alice</span></li><li class="active"><span>Edit</span></li></ul>'
+            end
+          end
+        end
+      end
+
+      context 'with a parent' do
+        let(:parent_class) { Organisation }
+        let(:parent) { Organisation.new }
+        let(:parent_and_collection_linkable) { true }
+        let(:parent_class_linkable) { true }
+        let(:subject_class_linkable) { true }
+
+        before do
+          allow(parent).to receive(:id).and_return(1)
+          allow(parent).to receive(:to_s).and_return('Sealink Travel Group')
+
+          allow(route_checker_class).to receive(:new).with(view_context).and_return route_checker
+          allow(route_checker).to receive(:linkable?).with([Organisation, User]).and_return parent_and_collection_linkable
+          allow(route_checker).to receive(:linkable?).with(Organisation).and_return parent_class_linkable
+          allow(route_checker).to receive(:linkable?).with(Person).and_return subject_class_linkable
+
+          allow(view_context).to receive(:url_for).with(parent_class).and_return('/organisations')
+          allow(view_context).to receive(:url_for).with(parent).and_return('/organisations/1')
+          allow(view_context).to receive(:url_for).with([parent, Person]).and_return('/organisations/1/people')
+          allow(view_context).to receive(:url_for).with([parent, User]).and_return('/organisations/1/users')
+          allow(view_context).to receive(:parent_object).and_return(parent)
+        end
+
+        context 'when not shallow' do
+          let(:subject_linkable) { false }
+
+          context 'on index route' do
+            let(:action) { 'show' }
+
+            it 'should infer crumbs: Sealink Travel Group / Users / Alice ' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/organisations/1">Sealink Travel Group</a></li><li><a href="/organisations/1/users">Users</a></li><li><span>Alice</span></li></ul>'
+            end
+          end
+
+          context 'on edit route' do
+            let(:action) { 'edit' }
+
+            it 'should infer crumbs: Sealink Travel Group / Users / Alice / Edit' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/organisations/1">Sealink Travel Group</a></li><li><a href="/organisations/1/users">Users</a></li><li><span>Alice</span></li><li class="active"><span>Edit</span></li></ul>'
+            end
+          end
+        end
+
+        context 'when shallow' do
+          context 'on index route' do
+            let(:action) { 'show' }
+
+            it 'should infer crumbs: Sealink Travel Group / Users / Alice ' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/organisations/1">Sealink Travel Group</a></li><li><span>Alice</span></li></ul>'
+            end
+          end
+
+          context 'on edit route' do
+            let(:action) { 'edit' }
+
+            it 'should infer crumbs: Sealink Travel Group / Users / Alice / Edit' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/organisations/1">Sealink Travel Group</a></li><li><a href="/users/1">Alice</a></li><li class="active"><span>Edit</span></li></ul>'
+            end
+          end
+        end
+
+        context 'when subject class is not linkable' do
+          let(:parent_and_collection_linkable) { false }
+          let(:subject_base_linkable) { false }
+          let(:subject_base_class_linkable) { false }
+
+          context 'on index route' do
+            let(:action) { 'show' }
+
+            it 'should infer crumbs: Sealink Travel Group / Users / Alice ' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/organisations/1">Sealink Travel Group</a></li><li><span>Alice</span></li></ul>'
+            end
+          end
+
+          context 'on edit route' do
+            let(:action) { 'edit' }
+
+            it 'should infer crumbs: Sealink Travel Group / Users / Alice / Edit' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><a href="/organisations/1">Sealink Travel Group</a></li><li><a href="/people/1">Alice</a></li><li class="active"><span>Edit</span></li></ul>'
+            end
+          end
+        end
+
+        context 'when not parent_class_linkable' do
+          let(:parent_class_linkable) { false }
+
+          context 'on index route' do
+            let(:action) { 'show' }
+
+            it 'should infer crumbs: Sealink Travel Group / Users / Alice ' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><span>Sealink Travel Group</span></li><li><span>Alice</span></li></ul>'
+            end
+          end
+
+          context 'on edit route' do
+            let(:action) { 'edit' }
+
+            it 'should infer crumbs: Sealink Travel Group / Users / Alice / Edit' do
+              expect(subject).to eq '<ul class="breadcrumb"><li><span>Sealink Travel Group</span></li><li><a href="/users/1">Alice</a></li><li class="active"><span>Edit</span></li></ul>'
+            end
+          end
         end
       end
     end
